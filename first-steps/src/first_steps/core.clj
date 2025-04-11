420251b9d557b1758c7194612f71c0fe9a26e999
ingmar <ingmania@protonmail.com>
2025-04-11 15:20:03 -0600

    window opening

--- first-steps/src/first_steps/core.clj
+++ first-steps/src/first_steps/core.clj
@@ -1,7 +1,14 @@
 (ns first-steps.core
+  (:import [com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application Lwjgl3ApplicationConfiguration]
+           [com.badlogic.gdx ApplicationAdapter])
   (:gen-class))
 
+(def config (new Lwjgl3ApplicationConfiguration))
+
+(def mygame (proxy [ApplicationAdapter] [] ))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Running a libgdx app"
   [& args]
-  (println "Hello, World!"))
+  (new Lwjgl3Application mygame config)
+  (println "Bye~"))
