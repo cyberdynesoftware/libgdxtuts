a7cb8b80c86cce7a9ae9b5700ddcd09d4ef6f3f4
ingmar <ingmania@protonmail.com>
2025-04-24 12:38:37 -0600

    start game (set game screen) mechanism added

--- bucket_drops_screen_extension/src/bucket_drops_screen_extension/game.clj
+++ bucket_drops_screen_extension/src/bucket_drops_screen_extension/game.clj
@@ -6,6 +6,8 @@
            [com.badlogic.gdx.utils.viewport FitViewport])
   (:gen-class))
 
+(def game-started (atom false))
+
 (def mygame (proxy [Game] []
               (create []
                 (reset! globals/sprite-batch (new SpriteBatch))
@@ -17,6 +19,14 @@
                                                                            (.getHeight Gdx/graphics))))))))
                 (globals/load-assets)
                 (.setScreen this (menu/screen)))
+              (render []
+                (when (and (not @game-started)
+                           @menu/start-game)
+                  (reset! game-started true)
+                  ;(.setScreen this (game-screen/screen))
+                  (println "start")
+                  nil)
+                (proxy-super render))
               (dispose []
                 (.dispose @globals/sprite-batch)
                 (.dispose @globals/font))))
--- bucket_drops_screen_extension/src/bucket_drops_screen_extension/main_menu.clj
+++ bucket_drops_screen_extension/src/bucket_drops_screen_extension/main_menu.clj
@@ -1,22 +1,28 @@
 (ns bucket-drops-screen-extension.main-menu
   (:require [bucket-drops-screen-extension.globals :as globals])
-  (:import [com.badlogic.gdx Gdx ScreenAdapter]
+  (:import [com.badlogic.gdx Gdx ScreenAdapter Input$Buttons]
            [com.badlogic.gdx.graphics Color]
            [com.badlogic.gdx.utils ScreenUtils])
   (:gen-class))
+
+(def start-game (atom false))
 
 (defn screen
   []
   (proxy [ScreenAdapter] []
     (render [delta]
-      (ScreenUtils/clear Color/BLACK)
-      (.apply (deref globals/viewport))
-      (let [matrix (.. @globals/viewport getCamera combined)]
-        (.setProjectionMatrix @globals/sprite-batch matrix))
-      (.begin @globals/sprite-batch)
-      (.draw @globals/font @globals/sprite-batch "Drops" (float 1) (float 4))
-      (when (.update globals/assets)
-        (.draw @globals/font @globals/sprite-batch "Click to play" (float 1) (float 3)))
-      (.end @globals/sprite-batch))
+      (let [assets-ready (.update globals/assets)]
+        (ScreenUtils/clear Color/BLACK)
+        (.apply (deref globals/viewport))
+        (let [matrix (.. @globals/viewport getCamera combined)]
+          (.setProjectionMatrix @globals/sprite-batch matrix))
+        (.begin @globals/sprite-batch)
+        (.draw @globals/font @globals/sprite-batch "Drop" (float 1) (float 4))
+        (when assets-ready
+          (.draw @globals/font @globals/sprite-batch "Click to play" (float 1) (float 3)))
+        (.end @globals/sprite-batch)
+        (when (and assets-ready 
+                   (.isButtonJustPressed Gdx/input Input$Buttons/LEFT))
+          (reset! start-game true))))
     (resize [x y]
       (.update @globals/viewport x y true))))
