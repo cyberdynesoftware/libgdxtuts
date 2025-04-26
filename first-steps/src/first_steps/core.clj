bacd263ac488a560eb2c11796f3e737231f59301
ingmar <ingmania@protonmail.com>
2025-04-26 12:35:25 -0600

    extended first steps to hello world

--- first-steps/src/first_steps/core.clj
+++ first-steps/src/first_steps/core.clj
@@ -1,14 +1,28 @@
 (ns first-steps.core
   (:import [com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application Lwjgl3ApplicationConfiguration]
+           [com.badlogic.gdx.graphics.g2d BitmapFont SpriteBatch]
            [com.badlogic.gdx ApplicationAdapter])
   (:gen-class))
 
-(def config (new Lwjgl3ApplicationConfiguration))
+(def msg "Hello Word")
+(def font (atom nil))
+(def sprite-batch (atom nil))
 
-(def mygame (proxy [ApplicationAdapter] [] ))
+(def config (doto (new Lwjgl3ApplicationConfiguration)
+              (.setTitle msg)
+              (.setWindowedMode 400 300)))
+
+(def mygame (proxy [ApplicationAdapter] []
+              (create []
+                (reset! font (new BitmapFont))
+                (reset! sprite-batch (new SpriteBatch)))
+              (render []
+                (.begin @sprite-batch)
+                (.draw @font @sprite-batch msg (float 100) (float 100))
+                (.end @sprite-batch))))
 
 (defn -main
-  "Running a libgdx app"
+  "A minimal libgdx app"
   [& args]
   (new Lwjgl3Application mygame config)
   (println "Bye~"))
