cf7411e2f2b1d859bc324e1d1d62dd3f365ae689
ingmar <ingmania@protonmail.com>
2025-04-29 12:19:38 -0600

    following the LWJGL snippet more precisely, but glfwKeyCallback still not working.

--- lwjgl_intro/src/lwjgl_intro/core.clj
+++ lwjgl_intro/src/lwjgl_intro/core.clj
@@ -1,6 +1,6 @@
 (ns lwjgl-intro.core
   (:import [org.lwjgl.glfw GLFW GLFWKeyCallbackI]
-           [org.lwjgl.opengl GL GL33]
+           [org.lwjgl.opengl GL GL11]
            [org.lwjgl.system MemoryUtil])
   (:gen-class))
 
@@ -14,21 +14,27 @@
   "LWJGL introduction"
   [& args]
   (GLFW/glfwInit)
-  (GLFW/glfwWindowHint GLFW/GLFW_CONTEXT_VERSION_MAJOR 3)
-  (GLFW/glfwWindowHint GLFW/GLFW_CONTEXT_VERSION_MINOR 3)
-  (GLFW/glfwWindowHint GLFW/GLFW_OPENGL_PROFILE GLFW/GLFW_OPENGL_CORE_PROFILE)
+  (GLFW/glfwDefaultWindowHints)
+  (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
+  (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_TRUE)
+
+  ;(GLFW/glfwWindowHint GLFW/GLFW_CONTEXT_VERSION_MAJOR 3)
+  ;(GLFW/glfwWindowHint GLFW/GLFW_CONTEXT_VERSION_MINOR 3)
+  ;(GLFW/glfwWindowHint GLFW/GLFW_OPENGL_PROFILE GLFW/GLFW_OPENGL_CORE_PROFILE)
 
   (let [window (GLFW/glfwCreateWindow (int 400) (int 300) "LWJGL" MemoryUtil/NULL MemoryUtil/NULL)]
     (GLFW/glfwMakeContextCurrent window)
+    (GLFW/glfwShowWindow window)
+
     (GL/createCapabilities)
-    (GL33/glClearColor (float 0) (float 0) (float 1) (float 0))
-    ;(GLFW/glfwSetKeyCallback window cb) 
+    (GL11/glClearColor (float 0) (float 0) (float 1) (float 0))
+    (GLFW/glfwSetKeyCallback window cb) 
 
     (while (not (GLFW/glfwWindowShouldClose window))
-      (when (= (GLFW/glfwGetKey window GLFW/GLFW_KEY_ESCAPE) GLFW/GLFW_PRESS)
-              (GLFW/glfwSetWindowShouldClose window true))
+    ;  (when (= (GLFW/glfwGetKey window GLFW/GLFW_KEY_ESCAPE) GLFW/GLFW_PRESS)
+    ;          (GLFW/glfwSetWindowShouldClose window true))
 
-      (GL33/glClear GL33/GL_COLOR_BUFFER_BIT)
+      (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
       (GLFW/glfwSwapBuffers window)
       (GLFW/glfwPollEvents)))
 
