b88c9df8b4a461e1ccda4dd605d4e33c0f58309b
ingmar <ingmania@protonmail.com>
2025-05-20 12:42:55 -0600

    first opengl triangle drawn

--- learnopengl/src/learnopengl/core.clj
+++ learnopengl/src/learnopengl/core.clj
@@ -16,17 +16,23 @@
   (let [window (GLFW/glfwCreateWindow (int 400) (int 300) "learnopengl" MemoryUtil/NULL MemoryUtil/NULL)]
     (GLFW/glfwMakeContextCurrent window)
     (GL/createCapabilities)
-    (GL33/glClearColor (float 0.2) (float 0.3) (float 0.3) (float 0))
 
-    (triangle/activate-shader-program)
+    (let [shader-program (triangle/get-shader-program)
+          vao (triangle/create-vertex-array)]
 
-    (while (not (GLFW/glfwWindowShouldClose window))
-      (when (= (GLFW/glfwGetKey window GLFW/GLFW_KEY_ESCAPE) GLFW/GLFW_PRESS)
-              (GLFW/glfwSetWindowShouldClose window true))
+      (while (not (GLFW/glfwWindowShouldClose window))
+        (when (= (GLFW/glfwGetKey window GLFW/GLFW_KEY_ESCAPE) GLFW/GLFW_PRESS)
+          (GLFW/glfwSetWindowShouldClose window true))
 
-      (GL33/glClear GL33/GL_COLOR_BUFFER_BIT)
-      (GLFW/glfwSwapBuffers window)
-      (GLFW/glfwPollEvents)))
+        (GL33/glClearColor (float 0.2) (float 0.3) (float 0.3) (float 1))
+        (GL33/glClear GL33/GL_COLOR_BUFFER_BIT)
+        
+        (GL33/glUseProgram shader-program)
+        (GL33/glBindVertexArray vao)
+        (GL33/glDrawArrays GL33/GL_TRIANGLES 0 3)
+
+        (GLFW/glfwSwapBuffers window)
+        (GLFW/glfwPollEvents))))
 
   (GLFW/glfwTerminate)
   (println "So long..."))
--- learnopengl/src/learnopengl/triangle.clj
+++ learnopengl/src/learnopengl/triangle.clj
@@ -7,8 +7,10 @@
   0.5 -0.5 0
   0 0.5 0])
 
-;(def vertex-buffer (doto (BufferUtils/createFloatBuffer (count vertices))
-;                     (dorun (map #(.put (float %)) vertices))))
+(def vertex-buffer
+  (doto (BufferUtils/createFloatBuffer (count vertices))
+    (.put (float-array vertices))
+    (.flip)))
 
 (defn create-vbo
   []
@@ -57,11 +59,22 @@
         (println (GL33/glGetProgramInfoLog program))))
     program))
 
-(defn activate-shader-program
+(defn get-shader-program
   []
   (let [vertex-shader (create-shader GL33/GL_VERTEX_SHADER vertex-shader-source)
         fragment-shader (create-shader GL33/GL_FRAGMENT_SHADER fragment-shader-source)
         shader-program (create-shader-program vertex-shader fragment-shader)]
-    (GL33/glUseProgram shader-program)
     (GL33/glDeleteShader vertex-shader)
-    (GL33/glDeleteShader fragment-shader)))
+    (GL33/glDeleteShader fragment-shader)
+    shader-program))
+
+(defn create-vertex-array
+  []
+  (let [vbo (GL33/glGenBuffers)
+        vao (GL33/glGenVertexArrays)]
+    (GL33/glBindVertexArray vao)
+    (GL33/glBindBuffer GL33/GL_ARRAY_BUFFER vbo)
+    (GL33/glBufferData GL33/GL_ARRAY_BUFFER vertex-buffer GL33/GL_STATIC_DRAW)
+    (GL33/glVertexAttribPointer 0 3 GL33/GL_FLOAT false 0 0)
+    (GL33/glEnableVertexAttribArray 0)
+    vao))
